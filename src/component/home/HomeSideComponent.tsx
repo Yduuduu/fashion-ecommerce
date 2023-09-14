1d20d48661b4f78371ac4f984383e9e590f02ba5
Yduuduu <hou_o@naver.com>
2023-09-14 18:04:32 +0900

    💄 사이드 메뉴 하단 정보파트 완료, 하트이미지 추가

--- src/component/home/HomeSideComponent.tsx
+++ src/component/home/HomeSideComponent.tsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import '../../pages/home/dist/HomePage.css';
 import { brandList, itemList } from 'data/SideBarItem';
 import icon_arrow_right from '../../assets/img/icon_arrow_right.svg';
+import icon_call from '../../assets/img/icon_call.svg';
+import icon_heart from '../../assets/img/icon_heart.svg';
+import { Link } from 'react-router-dom';
+import { Grid } from '@mui/material';
 
 // 사이드 네비게이션 컴포넌트
 function HomeSideComponent() {
@@ -59,9 +63,12 @@ function HomeSideComponent() {
           {brandList.map((item, idx) => {
             return (
               <div className="homePage-side__brand" key={idx}>
-                <span>{item.name}</span>
-                <br />
-                <span>{item.sub}</span>
+                <div className="homePage-side__brand__wrap">
+                  <span>{item.name}</span>
+                  <br />
+                  <span>{item.sub}</span>
+                </div>
+                <img src={icon_heart} alt="img" />
               </div>
             );
           })}
@@ -69,18 +76,37 @@ function HomeSideComponent() {
       )}
       <div className="homePage-side__bottom">
         <div className="homePage-side__bottom__call">
-          <span>1522-1234</span>
+          <h4>
+            <img src={icon_call} alt="img" />
+            1500-1234
+          </h4>
           <span>오전9시~오후6시 운영 / 토,일,휴일 휴무</span>
         </div>
         <p className="homePage-side__bottom__text">
-          <span>- 전화 전 자주 묻는 질문을 확인하세요.</span>
-          <span>- 1:1문의를 통해서도 상담이 가능합니다.</span>
+          <span>
+            - 전화 전 <Link to="/">자주 묻는 질문</Link>을 확인하세요.
+          </span>
+          <br />
+          <span>
+            - <Link to="/">1:1문의</Link>를 통해서도 상담이 가능합니다.
+          </span>
+          <br />
           <span>- 상품 문의는 각 상품 Q&A를 이용하세요.</span>
         </p>
         <ul className="homePage-side__bottom__button">
-          <li>회원후기</li>
-          <li>회원혜택</li>
-          <li>공지사항</li>
+          <Grid item xs={12}>
+            <Grid container spacing={1}>
+              <Grid item xs={4}>
+                <li>회원후기</li>
+              </Grid>
+              <Grid item xs={4}>
+                <li>회원혜택</li>
+              </Grid>
+              <Grid item xs={4}>
+                <li>공지사항</li>
+              </Grid>
+            </Grid>
+          </Grid>
         </ul>
       </div>
     </aside>
--- src/component/home/dist/HomeSideComponent.js
+++ src/component/home/dist/HomeSideComponent.js
@@ -4,6 +4,10 @@ var react_1 = require("react");
 require("../../pages/home/dist/HomePage.css");
 var SideBarItem_1 = require("data/SideBarItem");
 var icon_arrow_right_svg_1 = require("../../assets/img/icon_arrow_right.svg");
+var icon_call_svg_1 = require("../../assets/img/icon_call.svg");
+var icon_heart_svg_1 = require("../../assets/img/icon_heart.svg");
+var react_router_dom_1 = require("react-router-dom");
+var material_1 = require("@mui/material");
 // 사이드 네비게이션 컴포넌트
 function HomeSideComponent() {
     var sideList = [{ name: '품목' }, { name: '브랜드' }];
@@ -31,21 +35,38 @@ function HomeSideComponent() {
                     react_1["default"].createElement("img", { src: icon_arrow_right_svg_1["default"], alt: "arrow-right" }))));
         }))) : (react_1["default"].createElement(react_1["default"].Fragment, null, SideBarItem_1.brandList.map(function (item, idx) {
             return (react_1["default"].createElement("div", { className: "homePage-side__brand", key: idx },
-                react_1["default"].createElement("span", null, item.name),
-                react_1["default"].createElement("br", null),
-                react_1["default"].createElement("span", null, item.sub)));
+                react_1["default"].createElement("div", { className: "homePage-side__brand__wrap" },
+                    react_1["default"].createElement("span", null, item.name),
+                    react_1["default"].createElement("br", null),
+                    react_1["default"].createElement("span", null, item.sub)),
+                react_1["default"].createElement("img", { src: icon_heart_svg_1["default"], alt: "img" })));
         }))),
         react_1["default"].createElement("div", { className: "homePage-side__bottom" },
             react_1["default"].createElement("div", { className: "homePage-side__bottom__call" },
-                react_1["default"].createElement("span", null, "1522-1234"),
+                react_1["default"].createElement("h4", null,
+                    react_1["default"].createElement("img", { src: icon_call_svg_1["default"], alt: "img" }),
+                    "1500-1234"),
                 react_1["default"].createElement("span", null, "\uC624\uC8049\uC2DC~\uC624\uD6C46\uC2DC \uC6B4\uC601 / \uD1A0,\uC77C,\uD734\uC77C \uD734\uBB34")),
             react_1["default"].createElement("p", { className: "homePage-side__bottom__text" },
-                react_1["default"].createElement("span", null, "- \uC804\uD654 \uC804 \uC790\uC8FC \uBB3B\uB294 \uC9C8\uBB38\uC744 \uD655\uC778\uD558\uC138\uC694."),
-                react_1["default"].createElement("span", null, "- 1:1\uBB38\uC758\uB97C \uD1B5\uD574\uC11C\uB3C4 \uC0C1\uB2F4\uC774 \uAC00\uB2A5\uD569\uB2C8\uB2E4."),
+                react_1["default"].createElement("span", null,
+                    "- \uC804\uD654 \uC804 ",
+                    react_1["default"].createElement(react_router_dom_1.Link, { to: "/" }, "\uC790\uC8FC \uBB3B\uB294 \uC9C8\uBB38"),
+                    "\uC744 \uD655\uC778\uD558\uC138\uC694."),
+                react_1["default"].createElement("br", null),
+                react_1["default"].createElement("span", null,
+                    "- ",
+                    react_1["default"].createElement(react_router_dom_1.Link, { to: "/" }, "1:1\uBB38\uC758"),
+                    "\uB97C \uD1B5\uD574\uC11C\uB3C4 \uC0C1\uB2F4\uC774 \uAC00\uB2A5\uD569\uB2C8\uB2E4."),
+                react_1["default"].createElement("br", null),
                 react_1["default"].createElement("span", null, "- \uC0C1\uD488 \uBB38\uC758\uB294 \uAC01 \uC0C1\uD488 Q&A\uB97C \uC774\uC6A9\uD558\uC138\uC694.")),
             react_1["default"].createElement("ul", { className: "homePage-side__bottom__button" },
-                react_1["default"].createElement("li", null, "\uD68C\uC6D0\uD6C4\uAE30"),
-                react_1["default"].createElement("li", null, "\uD68C\uC6D0\uD61C\uD0DD"),
-                react_1["default"].createElement("li", null, "\uACF5\uC9C0\uC0AC\uD56D")))));
+                react_1["default"].createElement(material_1.Grid, { item: true, xs: 12 },
+                    react_1["default"].createElement(material_1.Grid, { container: true, spacing: 1 },
+                        react_1["default"].createElement(material_1.Grid, { item: true, xs: 4 },
+                            react_1["default"].createElement("li", null, "\uD68C\uC6D0\uD6C4\uAE30")),
+                        react_1["default"].createElement(material_1.Grid, { item: true, xs: 4 },
+                            react_1["default"].createElement("li", null, "\uD68C\uC6D0\uD61C\uD0DD")),
+                        react_1["default"].createElement(material_1.Grid, { item: true, xs: 4 },
+                            react_1["default"].createElement("li", null, "\uACF5\uC9C0\uC0AC\uD56D"))))))));
 }
 exports["default"] = HomeSideComponent;
